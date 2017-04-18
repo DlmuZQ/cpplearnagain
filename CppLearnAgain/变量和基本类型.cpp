64dac833ecb4ea462c33303bd370ee3edf11ce22
zhangqiang <zhangqianggis@sina.com>
2017-04-18 22:59:21 +0800

    add

--- CppLearnAgain/变量和基本类型.cpp
+++ CppLearnAgain/变量和基本类型.cpp
@@ -12,3 +12,14 @@ int main(){
 void* 是一种特殊的指针类型，可用于存放任意对象的地址；对其操作有限，只能和别的指针做比较、
 作为函数的输入，输出，或者赋值给另外的一个void*指针。
 */
+
+/*
+与const相关
+*/
+const int i = get_size();//在运行时初始化
+const int j = 42;//在编译时初始化
+
+double dval = 3.14;
+//中间隐式的类型转换，const int temp = dval
+//const int &v = temp;
+const int &v = dval;
