f9cbfa7c3b0e2ea04e66e78f89891cb6502d7f03
zhangqiang <zhangqianggis@sina.com>
2017-05-02 23:47:09 +0800

    a

--- CppLearnAgain/shell.sh
+++ CppLearnAgain/shell.sh
@@ -1,5 +1,5 @@
 1、du -h * | sort -nr > $HOME/script.txt
-#du 查询当前文件的使用空间，| 传递命令，将当前的结果传递给sort排序
+#du 查询当前文件的使用空间，| （管道）传递命令，将当前的结果传递给sort排序
 2、su -
 #普通用户转超级用户
 3、echo $SHELL
@@ -14,3 +14,8 @@
    !# #执行两次命令
 7、file [text_file]
 #检测文件类型
+8、lsattr [filename]
+   chattr [filename]
+#如果超级用户也不能删除的东西，用lsattr命令查看
+9、wall/write
+#wall向所有在线用户发送消息；write向指定用户发消息
