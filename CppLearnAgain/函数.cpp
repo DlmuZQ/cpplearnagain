cff8c00a103864904376e8fb245e55232a1f108a
zhangqiang <zhangqianggis@sina.com>
2017-05-04 20:44:15 +0800

    modify

--- CppLearnAgain/函数.cpp
+++ CppLearnAgain/函数.cpp
@@ -1,7 +1,6 @@
 函数的形参初始化的机理与变量初始化一样
 
 数组形参
-
 /*
 数组具有两个特殊的性质：
 1、不允许拷贝数组
@@ -27,4 +26,22 @@ void print(const int *beg,const int *end){
 //3、显示传递一个数组长度
 void print(const int a[],int size){
 
+}
+
+函数值是如何被返回的：返回一个值的方式和初始化一个变量或形参的方式完全一样；返回的值用于初始化调用点的一个临时变量
+
+不要返回局部对象的指针和引用：局部对象会销毁
+
+列表初始化返回值（C++11）
+vector<string> process()
+{
+	//
+	//...
+	//a和b为string对象
+	if(a == b)
+		return {};//返回一个空的vect对象
+	else if (a > b)
+		return {"hello","word"}; //返回vector初始化列表
+	else
+		return {"hello",a,b};
 }
